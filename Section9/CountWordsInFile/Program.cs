6f06b23e33e84da438494dc778b0a941d7cda441
Tríona Barrow <triona.barrow@paddypowerbetfair.com>
2020-07-27 12:05:57 +0100

    Complete section 9

---
+++ Section9/CountWordsInFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace CountWordsInFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("File.txt");
+            var count = 0;
+            try
+            {
+                var modInput = input.Replace(Environment.NewLine, " ");
+                var split = modInput.Split(' ');
+                count = split.Length;
+            } catch { }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ Section9/LongestWordInFile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace LongestWordInFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("File.txt");
+            var longestWord = string.Empty;
+            try
+            {
+                Regex pattern = new Regex(@"[,?!.\s]");
+                string modInput = pattern.Replace(input, " ");
+                var split = modInput.Split(' ');
+                foreach (var word in split)
+                {
+                    if(word.Trim().Length > longestWord.Length)
+                    {
+                        longestWord = word;
+                    }
+                }
+            }
+            catch { }
+            Console.WriteLine(longestWord);
+        }
+    }
+}
